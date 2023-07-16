e8586f3acdc8f60d78328cd221069a68f7f68bfc
Mursel Haxverdiyev <mursal.haqverdiyev@mail.ru>
2023-07-17 02:12:02 +0400

    add files

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".navbar-button");
+const menyu = document.querySelector(".linkler-menyu");
+button.addEventListener("click", function (){
+    menyu.classList.toggle("hidden")
+})
